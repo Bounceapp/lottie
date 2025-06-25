1ee97f370058864ae36480482967f59b96abf919
Ciaran Edwards <ciaran@usebounce.com>
2025-06-25 12:39:40 +0100

    feat: update expo to v53

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,13 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-babel",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
+  docs: {},
 }
